eb059bc27c11a68a4ec7b278bb950530ff547962
Anastasios Chanalaris <41963470+achanalaris@users.noreply.github.com>
2018-08-01 17:11:42 +0100

    Add files via upload

---
+++ selecting_columns.R
@@ -0,0 +1,31 @@
+##################################################################
+#####Selecting those genes that were significantly expressed######
+#### A1 is the file of the raw data that was tested with a t-test#
+#### tt1 is the file that has the t-test results##################
+###identify the ones with p<0.05 and write the new file A3########
+##################################################################
+A1<-read.csv("~/Documents/PH_SOMA_HIP/HIP_qn_raw_1.csv")
+
+tt1<-read.csv("~/Documents/PH_SOMA_HIP/t_test_hip_qn_adj.csv")
+
+head(tt1)
+
+tt2<-as.data.frame(t(tt1[,-1])) # drop 1st column and transpose tt1
+
+colnames(tt2)<-tt1$X # extract the column X and use it as names for tt2 columns
+
+A2<-rbind.data.frame(A1,tt2[2,]) # combine A1 and tt2
+
+A3<-data.frame(0,0) # initialise df A3
+
+for (i in 1:1310) {
+
+    if (A2[23,i] <= 0.05)
+  
+       A3<- cbind(A3,A2[1:22,i, drop=FALSE])
+
+    }
+
+A3<-A3[,-(1:2),drop=FALSE] # duplicated columns dropped
+
+write.csv(A3, "~/Documents/PH_SOMA_HIP/NEW/HIP_qn_sig_1.csv", col.names = TRUE, row.names = TRUE)
